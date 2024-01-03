6e7e05c8d6a6a3787cdec934aaa5a4983c0d33bd
saikrishna023 <gurusaik.akula@gmail.com>
2024-01-03 07:36:07 +0530

    upto dashboard..

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'SplashScreen/splash.dart';
-import 'models/firebase_options.dart';
+import 'firebase_options.dart';
+
 
 
 void main() async {
